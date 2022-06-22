4d35ec5325c506224b981936c85a3f968d3af4f9
Matheus-Missias <matheusfmissias@gmail.com>
2022-06-22 18:48:45 -0300

    Update LinearRegression

--- 4-Regression/LinearRegression.py
+++ 4-Regression/LinearRegression.py
@@ -5,10 +5,31 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
 # carrega os dados
-house_data = load_boston()
-X = house_data['data']
-y = house_data['target']
-df = pd.DataFrame(data=house_data.data, columns=house_data.feature_names)
+    #Dataset
+input_file = '0-Datasets/risk_factors_cervical_cancer_clear.csv'
+names = ['Age','Number of sexual partners','First sexual intercourse','Num of pregnancies','Smokes','Smokes (years)','Smokes (packs/year)','Hormonal Contraceptives','Hormonal Contraceptives (years)','IUD','IUD (years)',
+         'STDs','STDs (number)','STDs:condylomatosis','STDs:cervical condylomatosis','STDs:vaginal condylomatosis','STDs:vulvo-perineal condylomatosis','STDs:syphilis','STDs:pelvic inflammatory disease',
+         'STDs:genital herpes','STDs:molluscum contagiosum','STDs:AIDS','STDs:HIV','STDs:Hepatitis B','STDs:HPV','STDs: Number of diagnosis','Dx:Cancer',
+         'Dx:CIN','Dx:HPV','Dx','Hinselmann','Schiller','Citology','Biopsy'] 
+features = ['Age','Number of sexual partners','First sexual intercourse','Num of pregnancies','Smokes','Smokes (years)','Smokes (packs/year)','Hormonal Contraceptives','Hormonal Contraceptives (years)','IUD','IUD (years)',
+         'STDs','STDs (number)','STDs:condylomatosis','STDs:cervical condylomatosis','STDs:vaginal condylomatosis','STDs:vulvo-perineal condylomatosis','STDs:syphilis','STDs:pelvic inflammatory disease',
+         'STDs:genital herpes','STDs:molluscum contagiosum','STDs:AIDS','STDs:HIV','STDs:Hepatitis B','STDs:HPV','STDs: Number of diagnosis','Dx:Cancer',
+         'Dx:CIN','Dx:HPV','Dx','Hinselmann','Schiller','Citology']
+target = 'Biopsy'
+df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                 names = names)
+
+#Load dataset Digits
+data = df[features]
+target = df['Biopsy']
+labels = ['True label', 'Predective label']
+data.head()
+
+# Separate X and y data
+X = data
+y = target   
+
+df = pd.DataFrame(data=data, columns=labels)
 print(df.head())
 
 # separa em set de treino e teste
