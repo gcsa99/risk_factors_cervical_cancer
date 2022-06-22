26a0e369c64060460fb5723eeb8b7b1b9dd5ca78
Matheus-Missias <matheusfmissias@gmail.com>
2022-06-22 18:44:52 -0300

    DecisionTree

--- 3-Classification/DecisionTree.py
+++ 3-Classification/DecisionTree.py
@@ -1,32 +1,54 @@
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 def main():
-    iris = load_iris()
-    print(iris.data)
-    print(iris.target)
-    X = iris.data
-    y = iris.target
     
-    # Split the data - 75% train, 25% test
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)    
-    iris = load_iris()
-    print(iris.data)
-    print(iris.target)
-    X = iris.data
-    y = iris.target
+    # Faz a leitura do arquivo
+    input_file = '0-Datasets/risk_factors_cervical_cancer_clear.csv'
+    names = ['Age','Number of sexual partners','First sexual intercourse','Num of pregnancies','Smokes','Smokes (years)','Smokes (packs/year)','Hormonal Contraceptives','Hormonal Contraceptives (years)','IUD','IUD (years)',
+         'STDs','STDs (number)','STDs:condylomatosis','STDs:cervical condylomatosis','STDs:vaginal condylomatosis','STDs:vulvo-perineal condylomatosis','STDs:syphilis','STDs:pelvic inflammatory disease',
+         'STDs:genital herpes','STDs:molluscum contagiosum','STDs:AIDS','STDs:HIV','STDs:Hepatitis B','STDs:HPV','STDs: Number of diagnosis','Dx:Cancer',
+         'Dx:CIN','Dx:HPV','Dx','Hinselmann','Schiller','Citology','Biopsy'] 
+    features = ['Age','Number of sexual partners','First sexual intercourse','Num of pregnancies','Smokes','Smokes (years)','Smokes (packs/year)','Hormonal Contraceptives','Hormonal Contraceptives (years)','IUD','IUD (years)',
+         'STDs','STDs (number)','STDs:condylomatosis','STDs:cervical condylomatosis','STDs:vaginal condylomatosis','STDs:vulvo-perineal condylomatosis','STDs:syphilis','STDs:pelvic inflammatory disease',
+         'STDs:genital herpes','STDs:molluscum contagiosum','STDs:AIDS','STDs:HIV','STDs:Hepatitis B','STDs:HPV','STDs: Number of diagnosis','Dx:Cancer',
+         'Dx:CIN','Dx:HPV','Dx','Hinselmann','Schiller','Citology']
+    target = 'Biopsy'
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                     names = names) # Nome das colunas                      
+    #ShowInformationDataFrame(df,"Dataframe original")            
+   
+    # Separating out the features
+    X = df.loc[:, features].values
+    print(X.shape)
+
+    # Separating out the target
+    y = df.loc[:,[target]].values
+
+    # Standardizing the features
+    X = StandardScaler().fit_transform(X)
+    normalizedDf = pd.DataFrame(data = X, columns = features)
+    normalizedDf = pd.concat([normalizedDf, df[[target]]], axis = 1)
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    print(X_train.shape)
+    print(X_test.shape)
 
-    clf = DecisionTreeClassifier(max_leaf_nodes=3)
+    clf = DecisionTreeClassifier(max_leaf_nodes=10)
     clf.fit(X_train, y_train)
     tree.plot_tree(clf)
     plt.show()
     
+    predictions = clf.predict(X_test)
+    print(predictions)
     
+    result = clf.score(X_test, y_test)
+    print('Acuraccy:')
+    print(result)
 
 
 if __name__ == "__main__":
